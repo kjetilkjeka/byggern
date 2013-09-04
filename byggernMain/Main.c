27cb92386a39deec7c70105e57d507641872e458
kjetilkjeka <kjetilkjeka@gmail.com>
2013-09-04 20:21:03 +0200

    second assignment started
    The SRAM thingy is started. It's not in working condition.

--- byggernMain/Main.c
+++ byggernMain/Main.c
@@ -6,15 +6,29 @@
 #include <stdlib.h>
 #include "byggernlib/serialio.h"
 
+void SRAM_test();
+
 int main(void)
 {
+	
 	
 	USART_Init(MYUBRR);	
 	fdevopen(USART_Transmit, USART_Receive);
 	sei();
 	
 	USART_Transmit_String("sending fungerer, ");
-	printf("tester mottak\n");
+	printf("tester mottak    \n");
+	
+	MCUCR |= (1<<SRE); // setting SRE mode (wich will allow us to use the external memory interface)
+	
+	//freeing pin PC4-PC7 (A12-A15) for not for io (don't think this is nececarry?)
+	SFIOR &= ~((1<<XMM1)|(1<<XMM0));
+	SFIOR |= (1<<XMM2);
+	
+	
+	SRAM_test();
+	
+	
 	
 	while(1)
 	{
@@ -31,4 +45,30 @@ ISR(USART0_RXC_vect)
 	PORTB = 0;
 }
 
+
+void SRAM_test(void)
+{
+	volatile char *ext_ram = (char *) 0x1800; // Start address for the SRAM
+	unsigned int i, werrors, rerrors;
+	werrors = 0;
+	rerrors = 0;
+	unsigned char testvalue;
+	printf("Starting SRAM test...\r\n");
+	for (i = 1; i < 0x800; i++) {
+		testvalue = ~(i % 256);
+		ext_ram[i] = testvalue;
+		if (ext_ram[i] != testvalue) {
+			printf("SRAM error (write phase): ext_ram[%d] = %02X (should be %02X)\r\n", i, ext_ram[i], testvalue);
+			werrors++;
+		}
+	}
+	for (i = 0; i < 0x800; i++) {
+		testvalue = ~(i % 256);
+		if (ext_ram[i] != testvalue) {
+			printf("SRAM error (read phase): ext_ram[%d] = %02X (should be %02X)\r\n", i, ext_ram[i], testvalue);
+			rerrors++;
+		}
+	}
+	printf("SRAM test completed with %d errors in write phase and %d errors in read phase\r\n", werrors, rerrors);
+}
 
