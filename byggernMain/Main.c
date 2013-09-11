832f484bea47b64b7cef1f21c97a0feefe90304d
kjetilkjeka <kjetilkjeka@gmail.com>
2013-09-11 15:11:53 +0200

    second assignment done
    Some code tidying, SRAM header and everything works with GAL

--- byggernMain/Main.c
+++ byggernMain/Main.c
@@ -5,14 +5,15 @@
 #include <string.h>
 #include <stdlib.h>
 #include "byggernlib/serialio.h"
+#include "byggernlib/sram.h"
 
-void SRAM_test();
 
 int main(void)
 {
 	
 	
-	USART_Init(MYUBRR);	
+	USART_Init();	
+	SRAM_init();
 	fdevopen(USART_Transmit, USART_Receive);
 	sei();
 	
@@ -20,13 +21,7 @@ int main(void)
 	USART_Transmit_String("sending fungerer, ");
 	printf("tester mottak    \n");
 	
-	MCUCR |= (1<<SRE); // setting SRE mode (wich will allow us to use the external memory interface)
-	
-	//freeing pin PC4-PC7 (A12-A15) for not for io (don't think this is nececarry?)
-	SFIOR &= ~((1<<XMM1)|(1<<XMM0));
-	SFIOR |= (1<<XMM2);
-	
-	
+		
 	SRAM_test();
 	
 	while(1)
@@ -42,32 +37,5 @@ ISR(USART0_RXC_vect)
 {
 	USART_Transmit(USART_Receive());
 	PORTB = 0;
-}
-
-
-void SRAM_test(void)
-{
-	volatile char *ext_ram = (char *) 0x1800; // Start address for the SRAM
-	unsigned int i, werrors, rerrors;
-	werrors = 0;
-	rerrors = 0;
-	unsigned char testvalue;
-	printf("Starting SRAM test...\r\n");
-	for (i = 0; i < 0x800; i++) {
-		testvalue = ~(i % 256);
-		ext_ram[i] = testvalue;
-		if (ext_ram[i] != testvalue) {
-			printf("SRAM error (write phase): ext_ram[%d] = %02X (should be %02X)\r\n", i, ext_ram[i], testvalue);
-			werrors++;
-		}
-	}
-	for (i = 0; i < 0x800; i++) {
-		testvalue = ~(i % 256);
-		if (ext_ram[i] != testvalue) {
-			printf("SRAM error (read phase): ext_ram[%d] = %02X (should be %02X)\r\n", i, ext_ram[i], testvalue);
-			rerrors++;
-		}
-	}
-	printf("SRAM test completed with %d errors in write phase and %d errors in read phase\r\n", werrors, rerrors);
 }
 
---
+++ byggernMain/byggernlib/sram.h
@@ -0,0 +1,7 @@
+#ifndef SRAM_H
+#define SRAM_H
+
+void SRAM_test(void);
+void SRAM_init(void);
+
+#endif
